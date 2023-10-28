100d79ca17d1f84c17bd347279bc1dd160909d31
sam-riddhi <120332474+sam-riddhi@users.noreply.github.com>
2023-10-28 13:25:37 +0530

    Time: 5 ms (71.38%), Space: 42.1 MB (99.29%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0)
+        {
+            return false;
+        }
+        int rev = 0;
+        int temp = x;
+
+        while (temp != 0)
+        {
+            int digit = temp % 10;
+            rev = rev * 10 + digit;
+            temp = temp / 10;
+        }
+        return (rev == x);
+    }
+}
